06e14015a5cc6dd25344ab2c043e9d4ed018d6f7
Emile Jurgens <emile.jurgens@kcl.ac.uk>
2023-10-06 11:53:02 +0100

    fixed bug

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# thing model
+
+THING_MODEL = 'things.Thing'
